d8008c4752762aefb8e30d9fef77903ddae937ff
Jacob Heater <jacobheater@gmail.com>
2025-07-05 19:46:04 -0400

    Feature: Responsive UI
    - Adding styles in layout for consistent padding.
    - Making table and other components responsive.
    - Making layout in advocates view responsive.

--- src/app/advocate/[id]/page.tsx
+++ src/app/advocate/[id]/page.tsx
@@ -48,79 +48,77 @@ export default function AdvocateView() {
 
     return (
         <>
-            <div className="mt-24">
-                {loading && (
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <div className="text-center">
-                            Loading advocate data. Please wait...
-                        </div>
+            {loading && (
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="text-center">
+                        Loading advocate data. Please wait...
                     </div>
-                )}
-                {!advocate && !loading && (
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <div className="text-center">
-                            🥴 Sorry! We couldn't find an Advocate by that id. Please go back
-                            to the <Link href="/" className="underline text-[var(--solace-green)]">home</Link> page, and try searching for that
-                            advocate again.
-                        </div>
+                </div>
+            )}
+            {!advocate && !loading && (
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="text-center">
+                        🥴 Sorry! We couldn't find an Advocate by that id. Please go back
+                        to the <Link href="/" className="underline text-[var(--solace-green)]">home</Link> page, and try searching for that
+                        advocate again.
                     </div>
-                )}
-                {advocate && (
-                    <div className="w-[600px] mx-auto">
-                        <div className="w-[300px] h-[300px] border border-gray-300 mx-auto">
-                            <Image width={300} height={300} src="/profile.png" alt="Profile picture" priority />
-                        </div>
-                        <div className="flex flex-col w-full mt-10">
-                            {([{
-                                label: 'First Name',
-                                col: 'firstName'
-                            },
-                            {
-                                label: 'Last Name',
-                                col: 'lastName'
-                            },
-                            {
-                                label: 'Phone Number',
-                                col: 'phoneNumber'
-                            },
-                            {
-                                label: 'Degree',
-                                col: 'degree'
-                            },
-                            {
-                                label: 'Years of Experience',
-                                col: 'yearsOfExperience'
-                            },
-                            {
-                                label: 'City',
-                                col: 'city'
-                            }, {
-                                label: 'Specialties',
-                                col: 'specialties'
-                            }] as AdvocatePropsArray).map((item) => {
-                                
-                                let value: AdvocatePropValue = advocate[item.col];
+                </div>
+            )}
+            {advocate && (
+                <div className="w-full md:w-[80vw] mx-auto px-10 md:px-0">
+                    <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] border border-gray-300 mx-auto">
+                        <Image width={300} height={300} className="w-[200px] h-[200px] md:w-[300px] md:h-[300px]" src="/profile.png" alt="Profile picture" priority />
+                    </div>
+                    <div className="flex flex-col w-full mt-10">
+                        {([{
+                            label: 'First Name',
+                            col: 'firstName'
+                        },
+                        {
+                            label: 'Last Name',
+                            col: 'lastName'
+                        },
+                        {
+                            label: 'Phone Number',
+                            col: 'phoneNumber'
+                        },
+                        {
+                            label: 'Degree',
+                            col: 'degree'
+                        },
+                        {
+                            label: 'Years of Experience',
+                            col: 'yearsOfExperience'
+                        },
+                        {
+                            label: 'City',
+                            col: 'city'
+                        }, {
+                            label: 'Specialties',
+                            col: 'specialties'
+                        }] as AdvocatePropsArray).map((item) => {
 
-                                if (item.col === 'specialties') {
-                                    value = (value as ISpecialty[]).map((spc, i) => <div key={i} className="mb-2">
-                                        <Chip text={spc.title} />
-                                    </div>);
-                                }
+                            let value: AdvocatePropValue = advocate[item.col];
 
-                                return (
-                                    <div className="flex flex-row mb-4" key={item.col}>
-                                        <div className="font-bold">{item.label}</div>
-                                        <div className="ml-auto">{value as ReactNode}</div>
-                                    </div>
-                                );
-                            })}
-                        </div>
-                        <div className="text-center mt-10">
-                            <Button onClick={() => router.push("/")}>Back to Home Page</Button>
-                        </div>
+                            if (item.col === 'specialties') {
+                                value = (value as ISpecialty[]).map((spc, i) => <div key={i} className="mb-2">
+                                    <Chip text={spc.title} />
+                                </div>);
+                            }
+
+                            return (
+                                <div className="flex flex-row mb-4" key={item.col}>
+                                    <div className="font-bold">{item.label}</div>
+                                    <div className="ml-auto">{value as ReactNode}</div>
+                                </div>
+                            );
+                        })}
                     </div>
-                )}
-            </div>
+                    <div className="text-center mt-10">
+                        <Button onClick={() => router.push("/")}>Back to Home Page</Button>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
